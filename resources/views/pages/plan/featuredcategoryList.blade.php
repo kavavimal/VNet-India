c7047654f0c5cd595bddd88e118356ea05955548
Naman178 <114202935+rajivartifex@users.noreply.github.com>
2023-02-25 10:32:18 +0530

    Featured Category

--- resources/views/pages/plan/featuredcategoryList.blade.php
+++ resources/views/pages/plan/featuredcategoryList.blade.php
@@ -2,13 +2,13 @@
     <div class="card-body">
         <h4 class="mb-3">Featured Category</h4>
         <div class="featuredCategory_list_wrap">
-            @if(count($specifications) > 0)
-                @foreach ($specifications as $spec)
-                    <div class="form-check" id="featured-cat-{{$spec->id}}">
-                        <input class="form-check-input" type="checkbox" value="{{$spec->id}}" id="{{$spec->id}}">
-                        <label class="form-check-label mr-4 mb-2" for="{{$spec->id}}">{{$spec->spec_name}}</label>
-                        <button type="button" class="btn btn-outline-primary btn-sm edit-item mr-1" data-id="{{$spec->id}}" data-name="{{$spec->spec_name}}" data-toggle="modal" title="Edit"><i class="nav-icon i-pen-4"></i></button>
-                        <button type="button" class="btn btn-outline-primary btn-sm delete-item" data-id="{{$spec->id}}" data-name="{{$spec->spec_name}}" data-toggle="modal" title="Delete"><i class="nav-icon i-remove"></i></button>
+            @if(count($featuredCategory) > 0)
+                @foreach ($featuredCategory as $featured_cat)
+                    <div class="form-check" id="featured-cat-{{$featured_cat->id}}">
+                        <input class="form-check-input" type="checkbox" value="{{$featured_cat->id}}" id="{{$featured_cat->id}}">
+                        <label class="form-check-label mr-4 mb-2" for="featured-cat-{{$featured_cat->id}}">{{$featured_cat->featured_cat_name}}</label>
+                        <button type="button" class="btn btn-outline-primary btn-sm edit-item mr-1" data-id="{{$featured_cat->id}}" data-name="{{$featured_cat->featured_cat_name}}" data-toggle="modal" title="Edit"><i class="nav-icon i-pen-4"></i></button>
+                        <button type="button" class="btn btn-outline-primary btn-sm delete-item" data-id="{{$featured_cat->id}}" data-name="{{$featured_cat->featured_cat_name}}" data-toggle="modal" title="Delete"><i class="nav-icon i-remove"></i></button>
                     </div>
                 @endforeach
             @endif
