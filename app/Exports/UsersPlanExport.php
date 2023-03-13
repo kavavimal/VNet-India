d172563f4d67c1f47b3a96fe0930424066b551ea
Naman178 <114202935+rajivartifex@users.noreply.github.com>
2023-03-13 15:14:04 +0530

    excel

--- app/Exports/UsersPlanExport.php
+++ app/Exports/UsersPlanExport.php
@@ -2,17 +2,46 @@
 
 namespace App\Exports;
 
-use App\Models\UserPlan;
-use Illuminate\Contracts\View\View;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Facades\Excel;
 use Maatwebsite\Excel\Concerns\FromView;
-use Maatwebsite\Excel\Concerns\WithStyles;
-use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Events\AfterSheet;
+use Illuminate\Contracts\View\View;
 
 
-class UsersPlanExport implements FromView
+class UsersPlanExport implements FromView, WithEvents
 {
-    public function view(): View
+   public function view(): View
     {
-        return view('pages.user-plan.exportsPlan', [ 'plans' => UserPlan::where('sys_state','!=','-1')->get() ]);
+        // return view('pages.user-plan.exportsPlan', [ 'plans' => UserPlan::where('sys_state','!=','-1')->get() ]);
+        $data = [
+            [
+                'id' => 1,
+                'name' => 'John Doe',
+                'email' => 'johndoe@example.com',
+                'details' => '<table><tr><td>cell1</td><td>cell2</td><td>cell3</td></tr><tr><td>cell1</td><td>cell2</td><td>cell3</td></tr></table>',
+            ],
+            [
+                'id' => 2,
+                'name' => 'Naman Makwana',
+                'email' => 'naman@example.com',
+                'details' => '<table><tr><td>cell4</td><td>cell5</td><td>cell6</td></tr><tr><td>cell4</td><td>cell5</td><td>cell6</td></tr></table>',
+            ]           
+        ];
+
+        return view('pages.user-plan.exportsPlan', [
+            'plans' => $data,
+        ]);
     }
+
+    public function registerEvents(): array
+    {
+        return [
+            AfterSheet::class => function(AfterSheet $event) {
+                $event->sheet->mergeCells('A4:A5');
+            },
+        ];
+    }
+    
 }
--- resources/views/pages/user-plan/exportsPlan.blade.php
+++ resources/views/pages/user-plan/exportsPlan.blade.php
@@ -1,70 +1,20 @@
 <table>
     <thead>
-    <tr>
-        <th style="font-weight: bolder">Id</th>
-        <th style="font-weight: bolder">Sub Menu</th>
-        <th style="font-weight: bolder">Plan Name</th>
-        <th style="font-weight: bolder">Plan Pricing</th>
-        <th style="font-weight: bolder">Specification</th>
-        <th style="font-weight: bolder">Featured Category</th>
-        <th style="font-weight: bolder">Server Location</th>
-        <th style="font-weight: bolder">Billing Cycle</th>
-        <th style="font-weight: bolder">Support</th>
-        <th style="font-weight: bolder">Support Price</th>
-        <th style="font-weight: bolder">Total Price</th>
-    </tr>
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Details</th>
+        </tr>
     </thead>
     <tbody>
-    @foreach($plans as $plan)
-        <tr>
-            <td>{{$plan->id}}</td>
-            <td>{{ $plan->plan_product_id ? $plan->submenu->submenu_name : '' }}</td>
-            <td>{{ $plan->plan_name ?? '' }}</td>
-            <?php 
-                $plan_pricing  = '';
-                if(!empty($plan->plan_pricingids)){
-                    $plan_pricing_id = explode(",",$plan->plan_pricingids);
-                    $plan_pricing = \App\Models\PlanPricing::where('sys_state','!=','-1')->whereIn('id', $plan_pricing_id)->get();            
-                }                
-            ?>
-            <td>{{ $plan_pricing }}</td>
-            <?php 
-                $specifications  = '';
-                if(!empty($plan->specification)){
-                    $specifications_id = explode(",",$plan->specification);
-                    $specifications = \App\Models\Specification::whereIn('id',$specifications_id)->where('sys_state','!=','-1')->get();
-                }       
-            ?>
-            <td>{{ $specifications }}</td>
-            <?php 
-                $featured_category  = '';
-                if(!empty($plan->featured_category)){
-                    $featuredCategory_id = explode(",",$plan->featured_category);
-                    $featuredCategory = \App\Models\FeaturedCategory::where('sys_state','!=','-1')->whereIn('id',$featuredCategory_id)->with('children')->get();
-                }     
-            ?>
-            <td>{{ $featuredCategory }}</td>
-            <?php 
-                $featured_category  = '';
-                if(!empty($plan->featured_category)){
-                    $featuredCategory_id = explode(",",$plan->featured_category);
-                    $featuredCategory = \App\Models\FeaturedCategory::where('sys_state','!=','-1')->whereIn('id',$featuredCategory_id)->with('children')->get();
-                }
-            ?>
-            <td>{{ $featuredCategory }}</td>
-            <?php 
-                $bilingCycle  = '';
-                if(!empty($plan->billing_cycles)){
-                    $bilingCycle_id = explode(",",$plan->billing_cycles);
-                    $bilingCycle = \App\Models\BilingCycle::where('sys_state','!=','-1')->whereIn('id',$bilingCycle_id)->get();
-                }
-            ?>
-            <td>{{ $bilingCycle }}</td>
-            <td>{{ $plan->service_type_type ?? '' }}</td>
-            <td>{{ $plan->service_type_price ?? '' }}</td>
-            <td>{{ $plan->id ?? '' }}</td>
-
-        </tr>
-    @endforeach
+        @foreach ($plans as $row)
+            <tr>
+                <td>{{ $row['id'] }}</td>
+                <td>{{ $row['name'] }}</td>
+                <td>{{ $row['email'] }}</td>
+                <td>{!! html_entity_decode($row['details']) !!}</td>
+            </tr>
+        @endforeach
     </tbody>
 </table>
