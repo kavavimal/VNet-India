a29e1b98956c322281d24683f89787fd6c72f42d
Bhavesh <bhavesh.infinitysoftech@gmail.com>
2023-03-13 13:19:41 +0530

    Fix Total price calculation, fix billing cycle selections and final tax amount calculation

--- resources/views/pages/submenu/configuration/calAmountTax.blade.php
+++ resources/views/pages/submenu/configuration/calAmountTax.blade.php
@@ -4,24 +4,24 @@
         <div class="taxation_billing_list_wrap">
             <table class="table table-hover ">
                 <thead>
-                    <th>Name</th>
+                    <th>No Of Years</th>
                     <th>Amount</th>
                     <th>Tax</th>
                     <th>Final Amount</th>
                 </thead>
-                <tbody class="billing_price_table">
-                <tr class="first_year_info">
-                    <td>1 Year</td>
-                    <td class="default_amount"></td>
-                    <td>-</td>
-                    <td>-</td>
-                </tr>
-                <?php $taxTable = true; ?>
-                    @if(count($bilingCycle) > 0)
-                        @foreach ($bilingCycle as $list)
-                            @include('pages.submenu.configuration.taxationBillingCycleItem')
-                        @endforeach
-                    @endif
+                <tbody class="taxation_billing_price_table">
+                    <tr class="first_year_info">
+                        <td>1 Year</td>
+                        <td class="default_amount"></td>
+                        <td>-</td>
+                        <td>-</td>
+                    </tr>
+                    <?php $taxTable = true; ?>
+                        @if(count($bilingCycle) > 0)
+                            @foreach ($bilingCycle as $list)
+                                @include('pages.submenu.configuration.taxationBillingCycleItem')
+                            @endforeach
+                        @endif
                 </tbody>
             </table>        
         </div>        
